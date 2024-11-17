8e39964dc06d3a6dd7f529ee98f09c4f7499cc13
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-11-17 18:55:32 +0600

    34-1 Detailing the Functionality of the Reusable Doctor Modal

--- src/app/(withDashboardLayout)/dashboard/admin/doctors/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/doctors/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+
+import { Box, Button, Stack, TextField } from "@mui/material";
+import DoctorModal from "./components/DoctorModal";
+import { useState } from "react";
+
 const DoctorsPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
   return (
-    <div>
-      <h1>This is DoctorsPage</h1>
-    </div>
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Button onClick={() => setIsModalOpen(true)}>Create New Doctor</Button>
+        <DoctorModal open={isModalOpen} setOpen={setIsModalOpen} />
+        <TextField size="small" placeholder="Search Doctors" />
+      </Stack>
+    </Box>
   );
 };
 
